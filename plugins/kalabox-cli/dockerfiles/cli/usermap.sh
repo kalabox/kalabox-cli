955df9ea68e53e20ffbaa9095c20d957b01a9219
Mike Pirog <mike@kalamuna.com>
2016-03-16 13:27:47 -0700

    Ensure perms and ownership are correct for ssh keys

--- plugins/kalabox-cli/dockerfiles/cli/usermap.sh
+++ plugins/kalabox-cli/dockerfiles/cli/usermap.sh
@@ -4,6 +4,7 @@ set -e
 # Set defaults
 : ${KALABOX_UID:='1000'}
 : ${KALABOX_GID:='50'}
+: ${KALABOX_SSH_KEY:='id.rsa'}
 
 # Add local user to match host
 addgroup "$KALABOX_GID" || true
@@ -12,6 +13,18 @@ adduser -D -h "/config" -G "$KALABOX_GID" "$KALABOX_UID"
 # Move any config over and git correct perms
 export HOME=/config
 chown -Rf $KALABOX_UID:$KALABOX_UID /config
+
+# Check for an SSH key and move it into the correct position
+# We need to do this because VB SHARING ON WINDOZE will forward in
+# the ssh key as 777 ... yeah ... FACEPALM
+if [ -f "/user/.ssh/$KALABOX_SSH_KEY" ]; then
+  mkdir -p $HOME/.ssh
+  chmod 700 $HOME/.ssh
+  cp -f /user/.ssh/$KALABOX_SSH_KEY $HOME/.ssh/$KALABOX_SSH_KEY
+  chmod 600 $HOME/.ssh/$KALABOX_SSH_KEY
+  chown -Rf $KALABOX_UID:$KALABOX_UID $HOME/.ssh
+  ls -lsa $HOME/.ssh
+fi
 
 if [ -f "/${WEBROOT}/.stfolder" ]; then
   mv /${WEBROOT}/.stfolder /tmp/.stfolder
