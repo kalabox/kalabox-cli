0d61b7ca2aa3a4f6a8525176094b1e0c2a8d2099
rileysaurus <riley.cauldwell@gmail.com>
2016-03-24 15:36:23 -0700

    Added kbox.engine.events which returns a stream of docker events.

--- plugins/kalabox-engine-docker/index.js
+++ plugins/kalabox-engine-docker/index.js
@@ -311,8 +311,16 @@ module.exports = function(kbox) {
     });
   };
 
+  /*
+   * Returns a stream of docker events.
+   */
+  var events = function() {
+    return docker.events();
+  };
+
   return {
     build: build,
+    events: events,
     exists: exists,
     getProvider: getProvider,
     init: init,
